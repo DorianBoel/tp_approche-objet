3c5d2c8931c7668d6c54232d704727721878b826
DorianBoel <dorian.boel@free.fr>
2022-06-22 10:45:39 +0200

    add 'TP12 map Exercice 2'

--- src/fr/diginamic/maps/CreationMap.java
+++ src/fr/diginamic/maps/CreationMap.java
@@ -13,9 +13,28 @@ public class CreationMap {
 		salaryMap.put("Yu", 2250f);
 		salaryMap.put("Ingrid", 2015f);
 		salaryMap.put("Chantal", 2418f);
+
+		System.out.println("Taille de 'salaryMap': " + salaryMap.size());
+		System.out.println();
+
+		Map<Integer, String> cityMap = new HashMap<>();
+		cityMap.put(13, "Marseille");
+		cityMap.put(34, "Montpellier");
+		cityMap.put(44, "Nantes");
+		cityMap.put(75, "Paris");
+		cityMap.put(31, "Toulouse");
 		
-		System.out.println("Taille de la map: " + salaryMap.size());
+		cityMap.put(59, "Lille");
+		cityMap.put(33, "Lyon");
 		
+		for (int department : cityMap.keySet()) {
+			String city = cityMap.get(department);
+			System.out.println(city + " (" + department + ")");
+		}
+		System.out.println();
+		
+		System.out.println("Taille de 'cityMap': " + cityMap.size());
+
 	}
 
 }
